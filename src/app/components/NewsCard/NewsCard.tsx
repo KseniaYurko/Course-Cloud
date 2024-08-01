f6413b3797897996a11232839267141afbc588fc
Ksenia Yurko <ksyu257@gmail.com>
2024-08-02 01:01:32 +0300

    add: news card component

--- src/app/components/NewsCard/NewsCard.tsx
+++ src/app/components/NewsCard/NewsCard.tsx
@@ -5,7 +5,7 @@ import styles from './style.module.css'
 
 //import images
 import profile_photo from '@/public/images/mock_profile_photo.jpg'
-import card_cover from '@/public/images/mock_cover.jpg'
+import card_cover from '@/public/images/mock_cover_2.jpg'
 
 export default function NewsCard() {
     return (
@@ -16,12 +16,14 @@ export default function NewsCard() {
                     <div className={styles['news_card-heading-text']}>heading</div>
                 </div>
                 <div className={styles['news_card-body']}>
-                    <div className={styles['body-user_info']}>
-                        <div className={styles['body-user_info-photo']}>
-                            <Image src={profile_photo}  alt="profile_photo" className={styles['profile_photo']} />
+                    <div className={styles['body-user']}>
+                        <div className={styles['body-user-photo']}>
+                            <Image src={profile_photo}  alt="profile_photo" className={styles['profile_photo']} width={120}/>
                         </div>
-                        <div className={styles['body-user_info-name']}>Ksenia Yurko</div>
-                        <div className={styles['body-user_info-description']}>junior frontend developer</div>
+                        <div className={styles['body-user-info']}>
+                            <div className={styles['body-user-info_name']}>Ksenia Yurko</div>
+                            <div className={styles['body-user-info_description']}>junior frontend developer</div>
+                        </div>
                     </div>
                     <div className={styles['body-text']}>
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam fuga fugiat animi expedita ipsam, facere sint voluptate voluptatibus eos voluptatum?
@@ -29,6 +31,9 @@ export default function NewsCard() {
                 </div>
                 <div className={styles['body-cover']}>
                     <Image src={card_cover}  alt="cover_image" className={styles['cover_image']} />
+                </div>
+                <div className={styles['body-reactions']}>
+                    reactions
                 </div>
             </div>
         </>
--- src/app/pages/MainPage/MainPage.tsx
+++ src/app/pages/MainPage/MainPage.tsx
@@ -1,9 +1,19 @@
+//import components
 import NewsCard from "../../components/NewsCard/NewsCard"
+
+//import styles
+import styles from './style.module.css'
 
 export default function MainPage() {
     return (
-        <>
-            <NewsCard />
-        </>
+        <div className={styles['content-area']}>
+            <div className={styles['news-area']}>
+                <NewsCard />
+            </div>
+            <div className={styles['courses-area']}>
+
+            </div>
+            
+        </div>
     )
 }
