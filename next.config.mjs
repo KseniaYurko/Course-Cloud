583ed199b6260098e3bbe8863c83ece38533b28f
Ksenia Yurko <ksyu257@gmail.com>
2024-08-02 10:01:59 +0300

    test: gh-page display

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "export",
+    basePath: "/Course-Cloud",
+    images: {
+        unoptimized: true,
+    },
+};
 
 export default nextConfig;
